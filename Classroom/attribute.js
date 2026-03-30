d604c0ba21ff34bee1b76a31f55338a836cacd03
AyushPathak-630 <pathakayush564@gmail.com>
2026-03-31 01:48:14 +0530

    attributes in js

---
+++ Classroom/attribute.js
@@ -0,0 +1,27 @@
+// const element = document.getElementById("myH1");
+// let text = element.getAttribute("class");
+// document.getElementById("demo").innerHTML = text;
+
+// next is set attribute
+// function myFunction(){
+//     document.getElementById("myH1").setAttribute("class", "demoClass");
+//     alert('Attribute added successfully');
+// }
+function onClick(){
+    console.log("clicked");
+}
+function onMouseOver(){
+    console.log("ON mouse over");
+}
+function onMOuseOut(){
+    console.log("on mouse out");
+}
+function onMOuseUp(){
+    console.log("on mouse up");
+}
+function onMouseDOWN(){
+    console.log("on mouse down");
+}
+function onMouseMOve(){
+    console.log("on mouse out")
+}
