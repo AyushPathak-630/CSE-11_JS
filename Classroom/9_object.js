756651d45d09a2c3969efe3d664b88ebe6159767
AyushPathak-630 <pathakayush564@gmail.com>
2026-03-30 23:07:13 +0530

    objects done

---
+++ Classroom/9_object.js
@@ -0,0 +1,48 @@
+// let x = new object();
+
+// var person2 = new Object();
+// person2.name= 'ayush';
+// person2.age = 20;
+// person2.isMarried = true;
+// person2.address = {};
+// person2.address.street = 'nagar road';
+// person2['address']['flatNO'] = 33; // also it can be written like this
+// console.log(person2.age)
+
+// let person = {
+//     naem: 'John Mac',
+//     age = 33;
+//     // it can include array as well as any function
+// }
+
+// array is asn ordered collection of data (either primitive or object depending upon language)
+// it is special kind of an object
+// in js , collectionof anything
+
+// var myInfo = new Array(); 
+// var myInfo1 = Array();
+// console.log(myInfo, myInfo1);
+
+// let collection = [
+//     {},
+//     [],
+//     true,
+//     "John",
+//     function () {},
+//     876876097,
+//     undefined,
+//     null, 
+//     new String("abc"),
+//     new Date(),
+// ]
+// console.log(collection);
+// collection.teacherName = "ayush pathak";
+// collection.phoneNo = 875908770;
+// console.log(collection);
+// but it will not change the size of the array
+
+// js arrays are zero indexed, first element at 0 index and last at length - 1
+ 
+var trainerinfo = ["sdggsfdg",53, "Ghaziabad", {isMarried : true}];
+// console.log(trainerinfo);
+console.log(trainerinfo[1]);
