712988df0cf05a7ed5ff47793225da42aa55f7cf
AyushPathak-630 <pathakayush564@gmail.com>
2026-03-31 01:45:18 +0530

    learning in class

---
+++ Classroom/12.js
@@ -0,0 +1,18 @@
+// form events these are also very imp
+// focus 
+// const x = document.querySelector('input');
+// x.addEventListener('focus', onhandlefocus);
+// x.addEventListener('blur', onhandleblur);
+
+// function onhandlefocus(){
+//     x.style.background = "red";
+// }
+// function onhandleblur(){
+//     x.style.background = "yellow";
+//     x.style.borderColor = "green";
+// }
+
+// window events
+function myBody(){
+    alert("Window Loaded successfully");
+}
