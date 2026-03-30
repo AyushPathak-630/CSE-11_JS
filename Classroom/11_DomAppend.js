8886b617ce0ed5d64535a0811ca8a2532dcfd747
AyushPathak-630 <pathakayush564@gmail.com>
2026-03-30 23:12:37 +0530

    mastering dom in js

---
+++ Classroom/11_DomAppend.js
@@ -0,0 +1,19 @@
+// javascript can add new html elements and tags
+// create an Element
+const heading = document.createElement("h2");
+// get the parent
+const bodytag = document.getElementsByTagName("body");
+// add element at the position using append method bodytag[0].append
+// before, prepend, append, after
+(heading);
+heading.innerText = "hello"; 
+heading.style.backgroundColor = "red";
+// dom remove method
+// remove() method removes an
+
+// next is Event is Js
+// change in state of an object
+// 1. click    onclick   when mouse click on the element
+// 2. mouseover onmouseover when the cursor of the mouse comes over the element
+// 3. mouseout onmouseout  when the cursor of the mouse leaves an element
+// 4. mousedown  onmou    when the mouse button is pressed over the element
